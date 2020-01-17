41ac37e0f5ee488f652cfa0283db85eae91f8b4a
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-01-17 14:05:17 -0500

    Sabine Help - Rock movement
    I emailed Sabine for help regarding the movement of the rock. (See help_log folder for details).

--- projects/project_1/js/script.js
+++ projects/project_1/js/script.js
@@ -10,82 +10,92 @@ References:
 *********************************************************************/
 
 window.onload = setup;
-document.addEventListener('keydown', moveRock);
+document.addEventListener('keydown', handleKeyEvent);
 
-let rockX = 40;
-let rockY = 625;
-let rockR = 25;
-let rockS = 0;
-let rockE = 2 * Math.PI;
-let rockSpeed = 0;
+// let rockX = 40;
+// let rockY = 610;
+// let rockR = 35;
+// let rockS = 0;
+// let rockE = 2 * Math.PI;
+// let rockSpeed = 0;
+// SABINE ADD :: make these global
+let canvas;
+let canvasContext;
+let rock;
 
 
 function setup() {
   //createCanvas(windowWidth, windowHeight);
-  let canvas  = document.getElementById("canvas");
-  let rock = canvas.getContext('2d');
-//  canvas.style.backgroundColor = "#C5EFFD";
-  canvas.width = "1200";
+  canvas  = document.getElementById("canvas");
+  // SABINE :: YOU named this "rock" and i renamed it canvas Context - as it is not a rock it is the context that allows for drawing
+  canvasContext = canvas.getContext('2d');
+  canvas.style.backgroundColor = "#C5EFFD";
+  canvas.width = "1000";
   canvas.height = "650";
 
-  rock.beginPath();
-  rock.arc (rockX, rockY, rockR, rockS, rockE);
-  rock.fillStyle = "#5A4D41";
-  rock.fill();
-
-
-  let mountain = canvas.getContext('2d');
-  let mountainTop = canvas.getContext('2d');
-  let mountainBack = canvas.getContext('2d');
-
-  mountainBack.beginPath();
-  mountainBack.moveTo(500,550);
-  mountainBack.lineTo(1000,220);
-  mountainBack.lineTo(900,100);
-  mountainBack.fillStyle = "#461f2f";
-  mountainBack.fill();
-
-  mountain.beginPath();
-  mountain.moveTo(1200,650);
-  mountain.lineTo(300,650);
-  mountain.lineTo(1200,25);
-  mountain.fillStyle = "#311b25";
-  mountain.fill();
-
-  // mountainTop.beginPath();
-  // mountainTop.moveTo(1200,125);
-  // mountainTop.lineTo(1056,125);
-  // mountainTop.lineTo(1200,25);
-  // mountainTop.fillStyle = "#ffffff";
-  // mountainTop.fill();
-
-  rock.newPos = function() {
-  rock.rockX += rock.rockSpeed;
-  rock.rockY += rock.rockSpeed;
-  }
-
-  moveRock();
+  // make a rock:
+  rock = new Rock(40,610,35,0,2*Math.PI);
+  rock.displayRock();
+  requestAnimationFrame(animate);
 }
 
-// function update(){
-//
-// }
+function animate(){
+  //console.log("in animate");
+  canvasContext.clearRect(0,0,canvas.width,canvas.height);
+    rock.displayRock();
+  requestAnimationFrame(animate);
 
-function moveRock(){
-let rock = canvas.getContext('2d');
-rock.rockSpeed = 0;
+}
 
+function handleKeyEvent(){
+  rock.moveRock();
+  rock.updateRock();
+}
+
+
+// SABINE:: made a class for the rock object
+class Rock{
+  constructor(rockX, rockY, rockR, rockS, rockE){
+    this.rockX = rockX;
+    this.rockR = rockR;
+    this.rockY = rockY;
+    this.rockS = rockS;
+    this.rockE = rockE;
+    //have two speeds  - one for x and one for y
+    this.rockSpeedX =0.0;
+    this.rockSpeedY =0.0;
+    this.rockDirectionX = 1.0;
+    this.rockDirectionY =1.0;
+  }
+
+displayRock(){
+//  console.log("display");
+  canvasContext.beginPath();
+  canvasContext.arc (this.rockX, this.rockY, this.rockR,this.rockS,this.rockE);
+  canvasContext.fillStyle = "#5A4D41";
+  canvasContext.fill();
+  canvasContext.closePath();
+}
+
+updateRock(){
+  this.rockSpeedX = this.rockSpeedX*this.rockDirectionX;
+  this.rockX += this.rockSpeedX;
+  this.rockY += this.rockSpeedY;
+}
+
+
+moveRock(){
   if (event.keyCode === 39){
-  rock.rockSpeed += 1;
+  this.rockSpeedX =1;
+  this.rockDirectionX=1;
   console.log("moving right");
   }
 
-  if (event.keyCode === 37){
-  rock.rockSpeed -= 1;
+  if (event.keyCode=== 37){
+  this.rockSpeedX =1;
+  this.rockDirectionX=-1;
   console.log("moving left");
   }
 
-  else {
-  rock.rockSpeed = 0;
-  }
 }
+} //class
