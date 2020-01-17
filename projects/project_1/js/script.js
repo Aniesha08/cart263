cab4f9eb1b44bf74480955c7c4464c530f8edd8f
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-01-17 12:46:31 -0500

    Created Mountain

--- projects/project_1/js/script.js
+++ projects/project_1/js/script.js
@@ -13,8 +13,8 @@ window.onload = setup;
 document.addEventListener('keydown', moveRock);
 
 let rockX = 40;
-let rockY = 610;
-let rockR = 35;
+let rockY = 625;
+let rockR = 25;
 let rockS = 0;
 let rockE = 2 * Math.PI;
 let rockSpeed = 0;
@@ -24,14 +24,40 @@ function setup() {
   //createCanvas(windowWidth, windowHeight);
   let canvas  = document.getElementById("canvas");
   let rock = canvas.getContext('2d');
-  canvas.style.backgroundColor = "#C5EFFD";
-  canvas.width = "1000";
+//  canvas.style.backgroundColor = "#C5EFFD";
+  canvas.width = "1200";
   canvas.height = "650";
 
   rock.beginPath();
   rock.arc (rockX, rockY, rockR, rockS, rockE);
   rock.fillStyle = "#5A4D41";
   rock.fill();
+
+
+  let mountain = canvas.getContext('2d');
+  let mountainTop = canvas.getContext('2d');
+  let mountainBack = canvas.getContext('2d');
+
+  mountainBack.beginPath();
+  mountainBack.moveTo(500,550);
+  mountainBack.lineTo(1000,220);
+  mountainBack.lineTo(900,100);
+  mountainBack.fillStyle = "#461f2f";
+  mountainBack.fill();
+
+  mountain.beginPath();
+  mountain.moveTo(1200,650);
+  mountain.lineTo(300,650);
+  mountain.lineTo(1200,25);
+  mountain.fillStyle = "#311b25";
+  mountain.fill();
+
+  // mountainTop.beginPath();
+  // mountainTop.moveTo(1200,125);
+  // mountainTop.lineTo(1056,125);
+  // mountainTop.lineTo(1200,25);
+  // mountainTop.fillStyle = "#ffffff";
+  // mountainTop.fill();
 
   rock.newPos = function() {
   rock.rockX += rock.rockSpeed;
