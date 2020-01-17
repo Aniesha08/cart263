3106ccebbc8ab5753c0b5e929c34506cf905e4f4
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-01-17 10:30:51 -0500

    Updated global variable for rock
    - Rock movement not working

--- projects/project_1/js/script.js
+++ projects/project_1/js/script.js
@@ -12,41 +12,54 @@ References:
 window.onload = setup;
 document.addEventListener('keydown', moveRock);
 
+let rockX = 40;
+let rockY = 610;
+let rockR = 35;
+let rockS = 0;
+let rockE = 2 * Math.PI;
+let rockSpeed = 2;
+
+
 function setup() {
   //createCanvas(windowWidth, windowHeight);
   let canvas  = document.getElementById("canvas");
+  let rock = canvas.getContext('2d');
   canvas.style.backgroundColor = "#C5EFFD";
   canvas.width = "1000";
   canvas.height = "650";
 
-  displayRock();
+  rock.beginPath();
+  rock.arc (rockX, rockY, rockR, rockS, rockE);
+  rock.fillStyle = "#5A4D41";
+  rock.fill();
+
   moveRock();
 }
 
-function displayRock(){
-  let rock = canvas.getContext('2d');
-  let x = 40;
-  let y = 610;
-  let r = 35;
-  let s = 0;
-  let e = 2 * Math.PI;
-  rock.beginPath();
-  rock.arc(x, y, r, s, e);
-  rock.fillStyle = "#5A4D41";
-  rock.fill();
-}
+// function displayRock(){
+//   let x = 40;
+//   let y = 610;
+//   let r = 35;
+//   let s = 0;
+//   let e = 2 * Math.PI;
+//   rock.beginPath();
+//   rock.arc(x, y, r, s, e);
+//   rock.fillStyle = "#5A4D41";
+//   rock.fill();
+// }
 
 function moveRock(){
-  //right
+  let rock = canvas.getContext('2d');
+
   if (event.keyCode === 39){
-    rock.x = '+1';
-    // rock.style.transform = 'rotate('+1+'deg)';
-    // console.log("move right");
+  rock.vx = -rockSpeed;
   }
 
-  //left
-  if (event.keyCode === 37){
-      rock.style.transform = 'rotate('+-1+'deg)';
-      console.log("move left");
+    if (event.keyCode === 37){
+  rock.vx = rockSpeed;
+  }
+
+  else {
+  rock.vx = 0;
   }
 }
