49742ee5700ac7ac0db82069e1ad17726e625cec
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-01-21 15:11:27 -0500

    Added a span class of "Redacted"

---
+++ in-class/redactionist/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/********************************************************************
+
+Raving Redactionist
+Aniesha Sangarapillai
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+function setup() {
+
+}
