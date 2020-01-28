879b12977affd0634e9854ea6a614837fced6f29
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-01-28 17:15:29 -0500

    Cleaned up code for project 1
    Cleaned up code, added more comments.

--- projects/project_1/js/script.js
+++ projects/project_1/js/script.js
@@ -35,6 +35,7 @@ let rockSound;
 let timeOver;
 
 function setup() {
+  // Created audio elements
   rockSound = document.createElement("audio");
   rockSound.setAttribute("src","assets/sounds/rockroll.mp3");
   document.body.appendChild(rockSound);
@@ -45,24 +46,19 @@ function setup() {
   document.body.appendChild(timeOver);
   timeOver.pause();
 
-
+  // Created a canvas
   canvas  = document.getElementById("canvas");
-
-  // SABINE :: YOU named this "rock" and i renamed it canvas Context - as it is not a rock it is the context that allows for drawing
   canvasContext = canvas.getContext('2d');
   canvas.width = "1300";
   canvas.height = "650";
-  //canvas.style.background = "url ('../assets/images/sky_background.png')";
-  console.log(sec);
 
-  rockBg  = document.getElementById("rockie");
-
+  // Created objects rock & mountain
   mountainRight = new MountainRight (1300,650,800,650,1300,200);
-
+  rockBg  = document.getElementById("rockie");
   rock = new Rock(rockBg,30,620,60,60);
 
   requestAnimationFrame(animate);
-}
+} // End of setup
 
 function animate(){
   canvasContext.clearRect(0,0,canvas.width,canvas.height);
@@ -73,65 +69,65 @@ function animate(){
   mountainRight.displayMountainRight();
   rock.displayRock();
   requestAnimationFrame(animate);
-}
+} // End of animate
 
 function handleKeyEvent(){
+  //perform the following functions on keydown
   rock.moveRock();
   rock.updateRock();
   rock.stopRock();
-}
+} // End of handleKeyEvent
 
 function countDown(){
   sec--;
 
   if (sec <= 0) {
+  // when the countDown is over, reposition the rock to its starting position
+  // set the timer back to 25 seconds
     timeOver.play();
-  clearInterval(timer);
+    clearInterval(timer);
     rock.rockX = 30;
     rock.rockY = 620;
     timerStarted =false;
     sec = 25;
   }
-}
+} // End of countdown
 
 
 // SABINE:: made a class for the rock object
 class Rock{
 
-  constructor(rockBg, rockX, rockY, rockW, rockH){
-    this.rockBg = rockBg;
-    this.rockX = rockX;
-    this.rockY = rockY;
-    this.rockW = rockW;
-    this.rockH = rockH;
-    this.rockRot = 0;
+constructor(rockBg, rockX, rockY, rockW, rockH){
+  this.rockBg = rockBg;
+  this.rockX = rockX;
+  this.rockY = rockY;
+  this.rockW = rockW;
+  this.rockH = rockH;
+  this.rockRot = 0;
 
-    //have two speeds  - one for x and one for y
-    this.rockSpeedX =0.0;
-    this.rockSpeedY =0.0;
-    this.rockDirectionX = 1.0;
-    this.rockDirectionY =1.0;
-  }
+  // Have two speeds  - one for x and one for y
+  this.rockSpeedX =0.0;
+  this.rockSpeedY =0.0;
+  this.rockDirectionX = 1.0;
+  this.rockDirectionY =1.0;
+}
 
-  displayRock(){
-    canvasContext.save();
-    canvasContext.translate(this.rockX , this.rockY);
-    canvasContext.rotate(Math.PI / 180 * (this.rockRot += 0.0));
-    canvasContext.translate(-this.rockW/2.0, -this.rockH/2.0);
-    canvasContext.drawImage(this.rockBg, 0, 0, this.rockW, this.rockH);
-    canvasContext.restore();
-  }
+displayRock(){
+  canvasContext.save();
+  canvasContext.translate(this.rockX , this.rockY);
+  canvasContext.rotate(Math.PI / 180 * (this.rockRot += 0.0));
+  canvasContext.translate(-this.rockW/2.0, -this.rockH/2.0);
+  canvasContext.drawImage(this.rockBg, 0, 0, this.rockW, this.rockH);
+  canvasContext.restore();
+} // End of displayRock
 
 updateRock(){
   this.rockSpeedX = this.rockSpeedX*this.rockDirectionX;
   this.rockX += this.rockSpeedX;
   this.rockY += this.rockSpeedY;
-}
+} // End of updateRock
 
 moveRock(){
-// rock will move fast until it reaches the tip of the mountain
-// when the user presses the U key to move the rock up the mountain, the rock will go up really slow and never make it up on time.
-
   // RIGHT ARROW
   if (event.keyCode === 39){
   this.rockSpeedX =2;
@@ -158,13 +154,14 @@ moveRock(){
   }
 
   if(event.keyCode === 39){
+  // Start the timer only when right arrow key is pressed
     if(timerStarted ===false){
       timer = setInterval(countDown, 1000);
       timerStarted =true;
       console.log("started timer");
     }
   }
-}
+} // End of moveRock
 
 stopRock(){
   if (this.rockX <= 30){
@@ -176,7 +173,7 @@ stopRock(){
   }
 }
 
-} //class
+} // End of Rock class
 
 
 class MountainRight{
@@ -197,4 +194,4 @@ class MountainRight{
     canvasContext.fillStyle = "#66003d";
     canvasContext.fill();
   }
-}
+} // End of Mountain class
