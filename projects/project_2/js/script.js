8f0fc5fade6efa0629743c22aa745bc926c61f27
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-02-15 17:16:54 -0500

    P2: Loaded JSON data
    - Created data to load in JSON file
    - Loaded JSON data to javascript file
    - Verified in console if data is loaded

--- projects/project_2/js/script.js
+++ projects/project_2/js/script.js
@@ -2,7 +2,7 @@
 
 /*****************
 
-Title of Project
+Something is Wrong on the Internet
 Aniesha Sangarapillai
 
 This is a template. You must fill in the title,
@@ -13,5 +13,20 @@ author, and this description to match your project!
 $(document).ready(setup);
 
 function setup(){
-  
-}
+  $.getJSON("data/replace.json", dataLoaded);
+} // end of setup
+
+function dataLoaded(data) {
+  // defined variables for words to replace in lyrics
+  let insect = data.insect;
+  let place = data.place;
+  let liquid = data.liquid;
+  let noun = data.noun;
+  let verb = data.verb;
+
+  console.log(insect);
+  console.log(place);
+  console.log(liquid);
+  console.log(noun);
+  console.log(verb);
+} // end of dataLoaded
