5d87b24a252b1ee6f7d5c0e23dac80d648bb401b
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-02-15 10:54:25 -0500

    P2: Started Project 2
    Added the basic "document ready" function to start working on project.

---
+++ projects/project_2/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*****************
+
+Title of Project
+Aniesha Sangarapillai
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+$(document).ready(setup);
+
+function setup(){
+  
+}
