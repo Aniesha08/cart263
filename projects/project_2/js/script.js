98dd4cdfaa3662d932772ad157a7cf6e0c9e05b3
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-02-19 21:35:58 -0500

    P2: Random Surprise Egg Generation
    - Added image paths of surprise eggs as data in a JSON array as strings
    - Loaded the JSON array in Javascript
    - Generated a random function to pick a random image from the array
    - Set the variable as the source for the image to display when the user finishes saying the lyrics

--- projects/project_2/js/script.js
+++ projects/project_2/js/script.js
@@ -19,7 +19,9 @@ let noun;
 let verb;
 let container;
 let lyrics;
+let surpriseEgg;
 let $egg;
+
 // let $eggOpen;
 //let egg = document.getElementById("egg");
 
@@ -40,16 +42,20 @@ function dataLoaded(data) {
   verb = getRandomElement(data.verb);
   container = document.getElementById("lyrics_display");
   lyrics = document.createTextNode("Itsy-bitsy " + insect + " climbed up " + place + "," + " Down came the " + liquid + " and washed the " +
-              insect + " Out" + "," + " Out came the " + noun + " and " + verb + " up all the " + liquid + "," + " And the itsy-bitsy " + insect + " climbed up " + place + " again.");
+  insect + " Out" + "," + " Out came the " + noun + " and " + verb + " up all the " + liquid + "," + " And the itsy-bitsy " + insect + " climbed up " + place + " again.");
 
   container.appendChild(lyrics);
 
-/*  console.log(insect);
+  surpriseEgg = getRandomElement(data.surprise);
+  console.log(surpriseEgg);
+
+  /*  console.log(insect);
   console.log(place);
   console.log(liquid);
   console.log(noun);
   console.log(verb);
-  console.log(lyrics);*/
+  console.log(lyrics);
+  console.log("surpriseEgg");*/
 
 } // end of dataLoaded
 
@@ -59,24 +65,25 @@ function voiceCommands(){
     // let egg = document.getElementById("egg");
 
     let sayLyrics = {
-    '*lyrics': function() {
-    //'*insect':function(){
-      console.log(insect);
-    $egg.attr("src", "assets/images/egg2.png");
-    // egg.style.backgroundColor = "red";
-    console.log("saying lyrics");
-    }
+      '*lyrics': function() {
+        //'*insect':function(){
+        console.log(insect);
+        $egg.attr("src", surpriseEgg);
+        // $egg.attr("src", "assets/images/egg2.png");
+        // egg.style.backgroundColor = "red";
+        console.log("saying lyrics");
+      }
 
     }; // end of repeat
 
     console.log("annyang working");
     annyang.addCommands(sayLyrics);
     annyang.start();
-    }
+  }
 } // end of voiceCommands
 
 // Get a random element from the array
 function getRandomElement(array){
   let element = array[Math.floor(Math.random() * array.length)];
   return element;
- }
+}
