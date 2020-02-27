d0685109fbf8628da43f65ed1328224eca48f83f
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-02-27 14:51:39 -0500

    P2: Added setupGame() &restartGame()
    Reorganized code so that when the playAgain button is clicked, the code will reload a new set of lyrics.

--- projects/project_2/js/script.js
+++ projects/project_2/js/script.js
@@ -2,11 +2,12 @@
 
 /*****************
 
-Something is Wrong on the Internet
+Itsy Bitsy
 Aniesha Sangarapillai
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Itsy Bitsy is a kids game which mimics the Itsy Bitsy Spider rhyme. Instead, the lyrics of the rhyme are changed
+and make no sense, they are silly too! The user needs to say the lyrics of the rhuyme to pass each line.
+At the end of the third line, the egg will reveal a surprise!
 
 ******************/
 
@@ -31,38 +32,44 @@ let $egg;
 let sadEgg = "/assets/images/wrong.png";
 let happyEgg = "/assets/images/egg_closed.gif";
 
+// end of game variables
 let $congrats;
 let $playAgain;
 
+let dataIn;
+
+// The page will load the data from the json file
 function setup(){
   $.getJSON("data/replace.json", dataLoaded);
   $egg = $('#egg_closed');
 
   $congrats = $("#congrats");
-  $congrats.hide();
-
   $playAgain = $("#play_again");
-  $playAgain.hide();
 
   $playAgain.click(restartGame);
 } // end of setup
 
 
-// When the user clicks on the button, a new set of lyrics will load
+// When the user clicks on the Play Again button, the game will restart by revealing a new set of lyrics
 function restartGame(){
   console.log("button clicked");
-
-  // dataLoaded();
-}
+  setupGame();
+} // end of restartGame
 
 
 function dataLoaded(data){
-  // defined variables for words to replace in lyrics
-  insect = getRandomElement(data.insect);
-  place = getRandomElement(data.place);
-  liquid = getRandomElement(data.liquid);
-  noun = getRandomElement(data.noun);
-  verb = getRandomElement(data.verb);
+  dataIn = data;
+  setupGame();
+} // end of dataLoaded
+
+
+function setupGame(){
+  // get a random element from the variable's list
+  insect = getRandomElement(dataIn.insect);
+  place = getRandomElement(dataIn.place);
+  liquid = getRandomElement(dataIn.liquid);
+  noun = getRandomElement(dataIn.noun);
+  verb = getRandomElement(dataIn.verb);
 
   container = document.getElementById("lyrics_display");
 
@@ -76,14 +83,18 @@ function dataLoaded(data){
   let lyrics_three =  insect + " climbed up my " + place;
   thirdLine = document.createTextNode(" And the itsy bitsy " +lyrics_three+" again.");
 
-  surpriseEgg = getRandomElement(data.surprise);
-  // console.log(surpriseEgg);
+  surpriseEgg = getRandomElement(dataIn.surprise);
 
   voiceCommands(lyrics_one, lyrics_two, lyrics_three, container);
 
-} // end of dataLoaded
+  $congrats.hide();
+  $playAgain.hide();
+
+} // end of setupGame
 
 
+// Code for annyang voiceCommands: Through annyang commands, the following code will enable the user
+// to move on to the next line if they say part of the lyric correctly or try again if they don;t say it correctly
 function voiceCommands(lyrics_one, lyrics_two, lyrics_three, container){
 
   if (annyang) {
@@ -92,12 +103,12 @@ function voiceCommands(lyrics_one, lyrics_two, lyrics_three, container){
       '*tag Down came the :liquidA and clean the :insectA out': function(tag,liquidA,insectA) {
         console.log("insect:: "+insectA);
 
-      //'itsy bitsy :insectA climbed up my :placeA Down came the :liquidA and clean the :insectA out': function(insectA,placeA,liquidA) {
+        //'itsy bitsy :insectA climbed up my :placeA Down came the :liquidA and clean the :insectA out': function(insectA,placeA,liquidA) {
         // console.log(insect);
         // console.log(place);
         // console.log(liquid);
 
-      if(insect.toLowerCase() === insectA.toLowerCase() || liquid.toLowerCase() === liquidA.toLowerCase()){
+        if(insect.toLowerCase() === insectA.toLowerCase() || liquid.toLowerCase() === liquidA.toLowerCase()){
           $egg.attr("src", happyEgg);
           $(container).empty();
           container.appendChild(secondLine);
@@ -155,7 +166,6 @@ function voiceCommands(lyrics_one, lyrics_two, lyrics_three, container){
 
     }; // end of sayLyricsThree
 
-
     console.log("annyang working");
     annyang.addCommands(sayLyricsOne);
     annyang.addCommands(sayLyricsTwo);
@@ -165,8 +175,8 @@ function voiceCommands(lyrics_one, lyrics_two, lyrics_three, container){
   }
 } // end of voiceCommands
 
-// Get a random element from the array
+// Get a random element from the arrays in the json file
 function getRandomElement(array){
   let element = array[Math.floor(Math.random() * array.length)];
   return element;
-}
+} // end of getRandomElement
