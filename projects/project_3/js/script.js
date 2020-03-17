d89a2e14be1f8ff248f36dfb0d1f3a5fc3f087c3
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-03-17 15:13:52 -0400

    P3: Cleaned up code, changed code from DOM to JQuery

--- projects/project_3/js/script.js
+++ projects/project_3/js/script.js
@@ -13,39 +13,47 @@ author, and this description to match your project!
 $(document).ready(setup);
 
 function setup(){
-  openModal();
-}
+  toggleModal();
+} // end of setup
 
 
-function openModal(){
+
+function toggleModal(){
 // Get the modal
-let modal = document.getElementById("myModal");
+let $modal = $(".myModal");
 
-// Get the button that opens the modal
-let btn = document.getElementById("img_click"); // edited
+// Get the image that opens the modal
+let $imageClick = $(".img_click"); // edited
+
+let $comment = $(".comment");
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let $span = $(".close");
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// When the user clicks the image, open the modal
+$imageClick.click(function() {
+  console.log("clicked");
+  $modal.css("display", "block")
+});
+
+$comment.click(writeComment);
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+$span.click(function() {
+  $modal.css("display", "none")
+});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-}
+} // end of toggleModal
 
-function myFunction() {
-  var x = document.getElementById("myText").value;
-  document.getElementById("demo").innerHTML = x;
-}
+
+
+function writeComment() {
+  // take the value that is being typed in the input field
+  let $text = $(".myText").val();
+  let $commentDisplay = $(".display_comment")
+  // and display the comment in the display comment paragraph
+  $commentDisplay.css("margin-bottom", "1em");
+  $commentDisplay.append($text);
+  // clear the comment that was typed in the input after submitting the comment
+  $('input').val('');
+} // end of writeComment
