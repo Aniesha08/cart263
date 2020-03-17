6627c2918c4e43b3613c950cc2a0f9182d1edfeb
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-03-17 13:03:10 -0400

    P3: Modal Template (For Image View & Comments)
    Created a basic modal template using code from:
    https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
    and
    https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_text_get
    
    to create an image expansion and comment section like instagram's desktop version.

--- projects/project_3/js/script.js
+++ projects/project_3/js/script.js
@@ -2,10 +2,50 @@
 
 /*****************
 
-Title of Project
-Author Name
+Social Media
+Aniesha Sangarapillai
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+$(document).ready(setup);
+
+function setup(){
+  openModal();
+}
+
+
+function openModal(){
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("img_click"); // edited
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  document.getElementById("demo").innerHTML = x;
+}
