56c108376988e6b9a8bcd518faab2e5384c2db85
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-03-18 19:58:06 -0400

    P3: Login Page Conditions
    Added conditions for the login page:
    - the user has to type in a max of 10 charac. for the user & password
    - user will ony be able to login if they user & pass is greater than or equal to 6 characters

--- projects/project_3/js/script.js
+++ projects/project_3/js/script.js
@@ -16,67 +16,73 @@ function setup(){
 } // end of setup
 
 function logIn(){
-  // variables for username and password text fields
-  let $username = $("#username");
-  let $password = $("#password");
+  // variable for password text field which counts the length of its characters
+  let $password = $("#password").val().length;
+  // variable for login button
+  let $login = $("#login_button");
 
-  // let userlength = $("#username").val().length;
-  //
-  // let $login = $(login_button);
+  // When the user presses on the keyup...
+  $("#password").keyup(function() {
+    // increase the length value of the password character typed
+    $password++;
+    // if the user typed less that 6 characters for the password...
+    if ($password < 6){
+      // disable the click function of the login button
+      $login.off("click");
+    } // end of < 6 if statement
 
-  // if the user clicks on the username text field, clear the text field
-  $username.click(function() {
-    $username.val("");
-  });
+    // if the user typed greater than 6 characters for the password...
+    if ($password >= 6){
+      // change the background color of the login to button to suggest that it is clickable
+      $login.css("background-color", "#5aa8f9;");
+      // enable the click function for the login button
+      $login.click(function() {
+        // path for the login button to go to instagram account page
+        $(location).attr('href', 'https://aniesha08.github.io/cart263/projects/project_3/');
+      }); // end of login click function
+    } // end of > 6 if statement
+  }); // end of key up function
 
-  // if the user clicks on the password text field, clear the text field
-  $password.click(function() {
-    $password.val("");
-  });
-
-  // if (userlength > 6){
-  //    $login.css("background-color", "green");
-  //  }
-}
+} //end of logIn
 
 function toggleModal(){
-// Get the modal
-let $modal1 = $("#myModal1");
-let $modal2 = $("#myModal2");
+  // Get the modal
+  let $modal1 = $("#myModal1");
+  let $modal2 = $("#myModal2");
 
-// Get the image that opens the modal
-let $imageClick1 = $("#img_click1");
-let $imageClick2 = $("#img_click2");
+  // Get the image that opens the modal
+  let $imageClick1 = $("#img_click1");
+  let $imageClick2 = $("#img_click2");
 
-let $comment1 = $("#comment1");
-let $comment2 = $("#comment2");
+  let $comment1 = $("#comment1");
+  let $comment2 = $("#comment2");
 
-// Get the <span> element that closes the modal
-let $span1 = $("#close1");
-let $span2 = $("#close2");
+  // Get the <span> element that closes the modal
+  let $span1 = $("#close1");
+  let $span2 = $("#close2");
 
-// When the user clicks the image, open the modal
-$imageClick1.click(function() {
-  console.log("clicked");
-  $modal1.css("display", "block")
-});
+  // When the user clicks the image, open the modal
+  $imageClick1.click(function() {
+    console.log("clicked");
+    $modal1.css("display", "block")
+  });
 
-$imageClick2.click(function() {
-  console.log("clicked");
-  $modal2.css("display", "block")
-});
+  $imageClick2.click(function() {
+    console.log("clicked");
+    $modal2.css("display", "block")
+  });
 
-$comment1.click(writeComment1);
-$comment2.click(writeComment1);
+  $comment1.click(writeComment1);
+  $comment2.click(writeComment1);
 
-// When the user clicks on <span> (x), close the modal
-$span1.click(function() {
-  $modal1.css("display", "none")
-});
+  // When the user clicks on <span> (x), close the modal
+  $span1.click(function() {
+    $modal1.css("display", "none")
+  });
 
-$span2.click(function() {
-  $modal2.css("display", "none")
-});
+  $span2.click(function() {
+    $modal2.css("display", "none")
+  });
 
 } // end of toggleModal
 
@@ -95,19 +101,19 @@ $span2.click(function() {
 
 function writeComment1() {
   // for (let i = 1; i < 3; i++){
-    // take the value that is being typed in the input field
-    let $text = $(`#myText${commentNum}`).val();
-    let $commentDisplay1 = $(`#display_comment${commentNum}`);
+  // take the value that is being typed in the input field
+  let $text = $(`#myText${commentNum}`).val();
+  let $commentDisplay1 = $(`#display_comment${commentNum}`);
 
-    if (commentNum === 1){
-      console.log($text);
-    }
-    // and display the comment in the display comment paragraph
-    $commentDisplay1.css("margin-bottom", "1em");
-    $commentDisplay1.append($text);
-    // clear the comment that was typed in the input after submitting the comment
-    $('input').val('');
-    if (commentNum < 3) {
-      commentNum++;
-    }
+  if (commentNum === 1){
+    console.log($text);
+  }
+  // and display the comment in the display comment paragraph
+  $commentDisplay1.css("margin-bottom", "1em");
+  $commentDisplay1.append($text);
+  // clear the comment that was typed in the input after submitting the comment
+  $('input').val('');
+  if (commentNum < 3) {
+    commentNum++;
+  }
 } // end of writeComment
