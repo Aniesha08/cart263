6aac1c9eca54f8863f5126912be94b0dbbabe6dc
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-03-18 12:43:51 -0400

    P3: Created a login page
    -Created a login page
    -Need to create an if statement to count the number of characters that is typed to enable user to login...

--- projects/project_3/js/script.js
+++ projects/project_3/js/script.js
@@ -8,12 +8,36 @@ author, and this description to match your project!
 ******************/
 
 $(document).ready(setup);
+let commentNum = 1;
 
 function setup(){
+  logIn();
   toggleModal();
 } // end of setup
 
+function logIn(){
+  // variables for username and password text fields
+  let $username = $("#username");
+  let $password = $("#password");
 
+  let userlength = $("#username").val().length;
+
+  let $login = $(login_button);
+
+  // if the user clicks on the username text field, clear the text field
+  $username.click(function() {
+    $username.val("");
+  });
+
+  // if the user clicks on the password text field, clear the text field
+  $password.click(function() {
+    $password.val("");
+  });
+
+  // if (userlength > 6){
+  //    $login.css("background-color", "green");
+  //  }
+}
 
 function toggleModal(){
 // Get the modal
@@ -21,8 +45,8 @@ let $modal1 = $("#myModal1");
 let $modal2 = $("#myModal2");
 
 // Get the image that opens the modal
-let $imageClick1 = $("#img_click1"); // edited
-let $imageClick2 = $("#img_click2"); // edited
+let $imageClick1 = $("#img_click1");
+let $imageClick2 = $("#img_click2");
 
 let $comment1 = $("#comment1");
 let $comment2 = $("#comment2");
@@ -43,7 +67,7 @@ $imageClick2.click(function() {
 });
 
 $comment1.click(writeComment1);
-$comment2.click(writeComment2);
+$comment2.click(writeComment1);
 
 // When the user clicks on <span> (x), close the modal
 $span1.click(function() {
@@ -57,24 +81,34 @@ $span2.click(function() {
 } // end of toggleModal
 
 
-function writeComment1() {
-  // take the value that is being typed in the input field
-  let $text1 = $("#myText1").val();
-  let $commentDisplay1 = $("#display_comment1")
-  // and display the comment in the display comment paragraph
-  $commentDisplay1.css("margin-bottom", "1em");
-  $commentDisplay1.append($text1);
-  // clear the comment that was typed in the input after submitting the comment
-  $('input').val('');
-} // end of writeComment
+// function writeComment1() {
+//   // take the value that is being typed in the input field
+//   let $text1 = $("#myText1").val();
+//   let $commentDisplay1 = $("#display_comment1")
+//   // and display the comment in the display comment paragraph
+//   $commentDisplay1.css("margin-bottom", "1em");
+//   $commentDisplay1.append($text1);
+//   // clear the comment that was typed in the input after submitting the comment
+//   $('input').val('');
+// } // end of writeComment
 
-function writeComment2() {
-  // take the value that is being typed in the input field
-  let $text2 = $("#myText2").val();
-  let $commentDisplay2 = $("#display_comment2")
-  // and display the comment in the display comment paragraph
-  $commentDisplay2.css("margin-bottom", "1em");
-  $commentDisplay2.append($text2);
-  // clear the comment that was typed in the input after submitting the comment
-  $('input').val('');
+
+function writeComment1() {
+  // for (let i = 1; i < 3; i++){
+    // take the value that is being typed in the input field
+    let $text = $(`#myText${commentNum}`).val();
+    let $commentDisplay1 = $(`#display_comment${commentNum}`);
+
+    if (commentNum === 1){
+      console.log($text);
+    }
+    // and display the comment in the display comment paragraph
+    $commentDisplay1.css("margin-bottom", "1em");
+    $commentDisplay1.append($text);
+    // clear the comment that was typed in the input after submitting the comment
+    $('input').val('');
+    if (commentNum < 3) {
+      commentNum++;
+    }
+  // }
 } // end of writeComment
