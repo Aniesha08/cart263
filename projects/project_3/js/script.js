30faaafa665c1bb07a32fd629d9c5f7ea9e3ff14
Aniesha08 <31965770+Aniesha08@users.noreply.github.com>
2020-03-17 20:13:55 -0400

    P3: Added a 2nd modal
    Problem: Too much repetition of code, need to try to see if there is an easier way to do this with less duplication of code.

--- projects/project_3/js/script.js
+++ projects/project_3/js/script.js
@@ -1,13 +1,10 @@
 "use strict";
 
 /*****************
-
 Social Media
 Aniesha Sangarapillai
-
 This is a template. You must fill in the title,
 author, and this description to match your project!
-
 ******************/
 
 $(document).ready(setup);
@@ -20,40 +17,65 @@ function setup(){
 
 function toggleModal(){
 // Get the modal
-let $modal = $(".myModal");
+let $modal1 = $("#myModal1");
+let $modal2 = $("#myModal2");
 
 // Get the image that opens the modal
-let $imageClick = $(".img_click"); // edited
+let $imageClick1 = $("#img_click1"); // edited
+let $imageClick2 = $("#img_click2"); // edited
 
-let $comment = $(".comment");
+let $comment1 = $("#comment1");
+let $comment2 = $("#comment2");
 
 // Get the <span> element that closes the modal
-let $span = $(".close");
+let $span1 = $("#close1");
+let $span2 = $("#close2");
 
 // When the user clicks the image, open the modal
-$imageClick.click(function() {
+$imageClick1.click(function() {
   console.log("clicked");
-  $modal.css("display", "block")
+  $modal1.css("display", "block")
 });
 
-$comment.click(writeComment);
+$imageClick2.click(function() {
+  console.log("clicked");
+  $modal2.css("display", "block")
+});
+
+$comment1.click(writeComment1);
+$comment2.click(writeComment2);
 
 // When the user clicks on <span> (x), close the modal
-$span.click(function() {
-  $modal.css("display", "none")
+$span1.click(function() {
+  $modal1.css("display", "none")
+});
+
+$span2.click(function() {
+  $modal2.css("display", "none")
 });
 
 } // end of toggleModal
 
 
 
-function writeComment() {
+function writeComment1() {
   // take the value that is being typed in the input field
-  let $text = $(".myText").val();
-  let $commentDisplay = $(".display_comment")
+  let $text1 = $("#myText1").val();
+  let $commentDisplay1 = $("#display_comment1")
   // and display the comment in the display comment paragraph
-  $commentDisplay.css("margin-bottom", "1em");
-  $commentDisplay.append($text);
+  $commentDisplay1.css("margin-bottom", "1em");
+  $commentDisplay1.append($text1);
+  // clear the comment that was typed in the input after submitting the comment
+  $('input').val('');
+} // end of writeComment
+
+function writeComment2() {
+  // take the value that is being typed in the input field
+  let $text2 = $("#myText2").val();
+  let $commentDisplay2 = $("#display_comment2")
+  // and display the comment in the display comment paragraph
+  $commentDisplay2.css("margin-bottom", "1em");
+  $commentDisplay2.append($text2);
   // clear the comment that was typed in the input after submitting the comment
   $('input').val('');
 } // end of writeComment
